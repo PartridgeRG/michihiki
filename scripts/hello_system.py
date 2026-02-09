a5dd6a7ccd96edef142675bb495f5af8e6901d58
Richard Partridge <259996092+PartridgerRG@users.noreply.github.com>
2026-02-09 03:45:25 -0500

    Add hello_system diagnostic script

---
+++ scripts/hello_system.py
@@ -0,0 +1,7 @@
+import platform
+import sys
+
+print("Operating System : ", platform.system())
+print("OS Release       : ", platform.release())
+print("Architecture     : ", platform.machine())
+print("Python Version   : ", platform.python_version())
